d2eda3d3873f53bfa3060b851ce9703dbd100dbc
VSPaul <vascupaul_vsp@yahoo.com>
2020-02-06 17:29:14 +0200

    cardflip not ready

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import CardFlip from './components/cardFlip/CardFlip.js'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CardFlip/>
+      </div>
+    );
+  }
+}
+
+export default App;
