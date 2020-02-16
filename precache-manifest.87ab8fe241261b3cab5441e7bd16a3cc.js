86e33dd19dbd2885c65664563e2773b8c64811c5
Olivier Ggr <grandgerard.olivier@gmail.com>
2020-02-16 14:37:00 +0200

    Updates

---
+++ precache-manifest.87ab8fe241261b3cab5441e7bd16a3cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf3b53c5c56a6f3579d92684133d87f",
+    "url": "/cleanTechApplication/index.html"
+  },
+  {
+    "revision": "906609b0e7a38c404fad",
+    "url": "/cleanTechApplication/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "1af9a3e17118be27f796",
+    "url": "/cleanTechApplication/static/css/main.1e7848ce.chunk.css"
+  },
+  {
+    "revision": "906609b0e7a38c404fad",
+    "url": "/cleanTechApplication/static/js/2.e1218900.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cleanTechApplication/static/js/2.e1218900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af9a3e17118be27f796",
+    "url": "/cleanTechApplication/static/js/main.05c593cb.chunk.js"
+  },
+  {
+    "revision": "0b30a44c79d8a842365f",
+    "url": "/cleanTechApplication/static/js/runtime-main.e6ead5f0.js"
+  },
+  {
+    "revision": "4a5e5a830b2c479638e85e32ce5a04b6",
+    "url": "/cleanTechApplication/static/media/logo.4a5e5a83.png"
+  },
+  {
+    "revision": "0605a8a1ffa2c486fc0f34ccdf32f528",
+    "url": "/cleanTechApplication/static/media/yas2.0605a8a1.jpg"
+  }
+]);
